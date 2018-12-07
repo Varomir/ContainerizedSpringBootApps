ad5bbe6bdd1ce246a14426bea06b124b65805bd3
Vadym Feshchenko <VFeshchenko@ten-x.com>
2018-12-07 13:23:08 +0200

    add custom 'DataSource' bean

---
+++ src/main/java/org/mycompany/config/PersistenceConfig.java
@@ -0,0 +1,24 @@
+package org.mycompany.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class PersistenceConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        EmbeddedDatabaseBuilder builder = new EmbeddedDatabaseBuilder();
+        EmbeddedDatabase db = builder
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("myschema.sql")
+                .addScript("myData.sql")
+                .build();
+        return db;
+    }
+}
