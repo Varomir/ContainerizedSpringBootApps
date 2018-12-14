446ca5d863006a068c317694c9c8b462434ac73e
Vadym Feshchenko <VFeshchenko@ten-x.com>
2018-12-14 11:29:25 +0200

    Add custom health check implementation

---
+++ src/main/java/org/mycompany/HealthCheck.java
@@ -0,0 +1,23 @@
+package org.mycompany;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HealthCheck implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check();  //Perform some application specific health check logic
+        if (errorCode != 0) {
+            return Health.down().withDetail("Error Code", errorCode).build();
+        }
+        return Health.up().build();
+    }
+
+    public int check() {
+        //Your logic to check application health
+        return 0;
+    }
+}
